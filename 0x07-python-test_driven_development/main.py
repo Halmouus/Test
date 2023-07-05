e140424031f9f5e7253ec66f6ad48ed0e0daa254
Halmouus <125438976+Halmouus@users.noreply.github.com>
2023-07-05 00:43:32 +0000

    test driven yada yada

---
+++ 0x07-python-test_driven_development/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+matrix_divided = __import__('2-matrix_divided').matrix_divided
+
+matrix = [
+    [1, 2, 3, 5],
+    [4, 18, 6, 12]
+]
+print(matrix_divided(matrix, 2))
+print(matrix)
