b26b33cd4f0ef2702cc15e6661435d19a75a2fac
Halmouus <125438976+Halmouus@users.noreply.github.com>
2023-07-06 23:32:20 +0000

    tests

--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -2,4 +2,4 @@
 max_integer = __import__('6-max_integer').max_integer
 
 print(max_integer([5.1, 6.4]))
-print(max_integer([1, 3, 4, 2]))
+print(max_integer(["bozzo", "hey", "hello", "bonjour", "bye"]))
