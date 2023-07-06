8fd45eed79939cd0d8fe3c3591ea3f89ae8d41ab
Halmouus <125438976+Halmouus@users.noreply.github.com>
2023-07-06 19:58:53 +0000

    hash_t

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: pointer to the newly created hash table
+ */
+unsigned long int hash_djb2(const unsigned char *str) {
+    
+}
