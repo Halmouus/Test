929035b015cbbdff4bd1ed05f8dd867079297673
Halmouus <125438976+Halmouus@users.noreply.github.com>
2023-07-07 00:55:48 +0000

    hash hash

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,7 +1,29 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array
- * Return: pointer to the newly created hash table
+ * hash_table_print - prints  a hash table
+ * @ht: hash table
  */
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned long int index = 0;
+    int count = 0;
+    if (ht == NULL)
+        return;
+    printf("{");
+    for (index = 0; index < ht->size; index++)
+    {
+        if (ht->array[index])
+        {
+           if (count)
+           {
+               printf(", ");
+           }
+            printf("'%s': '%s'", ht->array[index]->key, ht->array[index]->value);
+            count++;
+        }
+                
+    }
+    printf("}\n");
+    
+}
