1f011f024f748251dcd6a36cffdf5b7f9a6337a5
Halmouus <125438976+Halmouus@users.noreply.github.com>
2023-07-07 01:16:27 +0000

    lhash hash still leaking

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,7 +1,25 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array
- * Return: pointer to the newly created hash table
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to delete
  */
+void hash_table_delete(hash_table_t *ht)
+{
+    unsigned long int index;
+    hash_node_t *temp = NULL;
+    if (ht == NULL)
+        return;
+    for (index = 0; index < ht->size; index++)
+    {
+        temp = ht->array[index];
+        while(temp)
+        {
+            free(temp->key);
+            free(temp->value);
+            temp = temp->next;
+        }
+        free(ht->array[index]);
+    }
+    free(ht);
+}
