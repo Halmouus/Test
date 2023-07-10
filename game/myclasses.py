c918792dcd5ad025aecf90b66ee7d31a34b065ce
Halmouus <125438976+Halmouus@users.noreply.github.com>
2023-07-10 01:25:43 +0000

    gamin'

--- game/myclasses.py
+++ game/myclasses.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 class Weapon():
-    def __init__(self, name, damage=0):
+    def __init__(self, name, damage=0, wear=10):
         self.__name = name
         self.__damage = damage
+        self.__wear = wear
     
     def get_damage(self):
         return self.__damage
     
+    def get_wear(self):
+        return self.__wear
+    
     def get_name(self):
         return self.__name
+    
+    def set_wear(self ,val):
+        self.__wear += val
+    
+    def __del__(self):
+        print(f"{self.__name} just disappeared!")
 
 
 class Hero():
@@ -25,37 +35,52 @@ class Hero():
         Hero.hero_number += 1
         Hero.hero_collection.append(self)
         print(f"{self.__name} created succesfully!")
+
+    def is_alive(self):
+        return self.__is_alive
     
     def info(self):
         print(f"{self.__name}\nLP: {self.__life}\nCoins: {self.__coin}")
 
     def gain_life(self, val):
-        self.__life += val
-        print(f"{self.__name} gained {val} life points!")
+        if self.is_alive() == True:
+            self.__life += val
+            print(f"{self.__name} gained {val} life points!")
 
     def lose_life(self, val):
-        if val > self.__life:
-            val = self.__life
-        print(f"{self.__name} lost {val} life points!")
-        self.__life -= val
-        if self.__life == 0:
-            self.__is_alive = False
-            self.__del__()
+        if self.is_alive() == True:
+            if val > self.__life:
+                val = self.__life
+            print(f"{self.__name} lost {val} life points!")
+            self.__life -= val
+            if self.__life == 0:
+                self.__is_alive = False
+                self.__del__()
     
     def equip_weapon(self, weapon):
-         self.__weapon = weapon
+        if self.is_alive() == True:
+            self.__weapon = weapon
     
     def attack(self, enemy):
-        print(f"{self.__name} attacks {enemy.get_name()} with {self.__weapon.get_name()}")
-        enemy.lose_life(self.__weapon.get_damage())
+        if enemy.is_alive() == True:
+            if self.__weapon is not None:
+                if self.__weapon.get_wear() > 0:
+                    print(f"{self.__name} attacks {enemy.get_name()} with {self.__weapon.get_name()}")
+                    enemy.lose_life(self.__weapon.get_damage())
+                    self.__weapon.set_wear(-1)
+                if self.__weapon.get_wear() == 0:
+                    self.__weapon.__del__()
+                    self.equip_weapon(None)
+        else:
+            print(f"{enemy.get_name()} already dead!")
     
     def get_name(self):
         return self.__name
     
     def __del__(self):
-        if self.__is_alive == False:
+        #if self.__is_alive == False:
             Hero.hero_number -= 1
-            #Hero.hero_collection.remove(self)
+            Hero.hero_collection.remove(self)
             print(f"{self.__name} is dead!")
 
     @classmethod
@@ -67,13 +92,22 @@ class Hero():
         print("----------------------------")
 
                 
-sword = Weapon("Sword", 30)
+sword = Weapon("Sword", 5)
 hero1 = Hero("Hero 1")
 hero2 = Hero("Hero 2")
 Hero.glob()
 
 hero1.gain_life(20)
 hero1.equip_weapon(sword)
+hero1.attack(hero2)
+hero1.attack(hero2)
+hero1.attack(hero2)
+hero1.attack(hero2)
+hero1.attack(hero2)
+hero1.attack(hero2)
+hero1.attack(hero2)
+hero1.attack(hero2)
+hero1.attack(hero2)
 hero1.attack(hero2)
 hero1.attack(hero2)
 hero1.attack(hero2)
