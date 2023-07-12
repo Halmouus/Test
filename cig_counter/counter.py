8a26f8999f08bd4c5e459316c6c4f352902fd880
Zameul <hotel.washington95@gmail.com>
2023-07-12 20:38:08 +0000

    smoking scriot

---
+++ cig_counter/counter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from myclasses import Pack
+
+pack1 = Pack("LM Blue", 27)
+print(pack1)
+pack1.smoke()
+print(pack1)
--- cig_counter/myclasses.py
+++ cig_counter/myclasses.py
@@ -6,8 +6,9 @@ class Pack():
     lost = 0
     landed = 0
     
-    def __init__(self, brand="Undefined", size=20, price, label=""):
+    def __init__(self, brand="Undefined", price=None, size=20, label=""):
         self.__brand = brand
+        self.__initsize = size
         self.__size = size
         self.__price = price
         self.__label = label
@@ -20,22 +21,37 @@ class Pack():
     def get_cig_left(self):
         return self.__size
 
-    def smoke(self, num="1"):
+    def smoke(self, num=1):
         self.__size -= num
         self.__cost += (self.__unitc * num)
         self.__smoked += num
+        return (self.__unitc * num)
 
-    def land(self, num="1"):
+    def land(self, num=1):
         self.__size -= num
         self.__cost += (self.__unitc * num)
         self.__landed += num
+        return (self.__unitc * num)
 
-    def lose(self, num="1"):
+    def lose(self, num=1):
         self.__size -= num
         self.__cost += (self.__unitc * num)
         self.__lost += num
+        return (self.__unitc * num)
     
     def get_cost(self):
+        return self.__initsize - self.__size
+
+    def __str__(self):
+        smoked = self.__initsize - self.__size
+        pack = ""
+        pack += f"Label : {self.__brand} \n"
+        pack += f"Remaining cigarettes : {self.__size}\n"
+        pack += f"Cost of smoked cigarettes : {smoked * self.__unitc}"
+        return pack
 
 class Cigarette(Pack):
-    def __init__(self,)
+    pass
+
+class Smoker():
+    pass
